931edc3aa6ce4949d173e23947f70c0f0ada0255
Lmostefaoui8 <lm87@njit.edu>
2025-08-13 21:06:39 +0500

    create hello world

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Assignments\rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Assignments\rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Assignments\rust\hello_world\target\debug\hello_world.exe: C:\Assignments\rust\hello_world\src\main.rs
